e2da82f9e4d4a3a843bfb03487c5c004e1102b39
jrubiaaaa <rubiaj3206@gmail.com>
2024-05-09 00:33:20 +0300

    Revert "searchbox"
    This reverts commit 731811d7aebdc6a885c9b67414771a73eec25c00.

--- filmfan/src/components/MovieList.js
+++ filmfan/src/components/MovieList.js
@@ -1,22 +1,23 @@
 const MovieList = (props) => {
-  const FavouriteComponent = props.favouriteComponent;
-
-  return (
-    <>
-      {props.movies.map((movie, index) => (
-        <div className="image-container d-flex justify-content-start m-3">
-          <img src={movie.Poster} alt="movie"></img>
-
-          <div
-            onClick={() => props.handleFavouritesClick(movie)}
-            className="overlay d-flex align-items-center justify-content-center"
-          >
-            <FavouriteComponent />
+    const FavouriteComponent = props.favouriteComponent;
+  
+    return (
+      <>
+        {props.movies.map((movie, index) => (
+          <div className="image-container d-flex justify-content-start m-3">
+            <img src={movie.Poster} alt="movie"></img>
+  
+            
+            <div
+              onClick={() => props.handleFavouritesClick(movie)}
+              className="overlay d-flex align-items-center justify-content-center"
+            >
+              <FavouriteComponent />
+            </div>
           </div>
-        </div>
-      ))}
-    </>
-  );
-};
-
-export default MovieList;
+        ))}
+      </>
+    );
+  };
+  
+  export default MovieList;
