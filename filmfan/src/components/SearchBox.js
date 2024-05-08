731811d7aebdc6a885c9b67414771a73eec25c00
jrubiaaaa <rubiaj3206@gmail.com>
2024-05-09 00:04:14 +0300

    searchbox

---
+++ filmfan/src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBox = (props) => {
+  return (
+    <div className="col col-sm-4">
+      <input
+        className="form-control"
+        value={prop.value}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Type to search"
+      ></input>
+    </div>
+  );
+};
+
+export default SearchBox;
