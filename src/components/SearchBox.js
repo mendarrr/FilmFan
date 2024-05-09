787d2d55a7141acfd741beb7803487d590a60906
brandondando <dandobrandon0@gmail.com>
2024-05-09 09:12:22 +0300

    commit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,12 +5,11 @@ const SearchBox = (props) => {
     <div className="col col-sm-4">
       <input
         className="form-control"
-        value={prop.value}
+        // value={prop.value}
         onChange={(event) => props.setSearchValue(event.target.value)}
         placeholder="Type to search"
       ></input>
     </div>
   );
 };
-
 export default SearchBox;
