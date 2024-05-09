a9a25dd5575352fae0b785942bc2122d2db09005
mendarrr <sarahabby541@gmail.com>
2024-05-09 20:39:15 +0300

    Updating code

--- filmfan/src/components/SearchBox.js
+++ filmfan/src/components/SearchBox.js
@@ -5,18 +5,22 @@ import { Link } from "react-router-dom";
 const SearchBox = (props) => {
   return (
     <div className="col col-sm-4">
-      <input
-        className="form-control"
-        value={props.value}
-        onChange={(event) => props.setSearchValue(event.target.value)}
-        placeholder="Type to search....."
-      />
-      <Link to={"/add-movie"} className="btn btn-primary">
-        Add a Movie
-      </Link>
-      <Link to={"/favourites"} className="btn btn-primary">
-        Favourites
-      </Link>
+      <div className="search-bar">
+        <input
+          className="form-control search-input"
+          value={props.value}
+          onChange={(event) => props.setSearchValue(event.target.value)}
+          placeholder="Type to search....."
+        />
+      </div>
+      <div className="links">
+        <Link to={"/add-movie"} className="btn btn-primary">
+          Add a Movie
+        </Link>
+        <Link to={"/favourites"} className="btn btn-primary">
+          Favourites
+        </Link>
+      </div>
     </div>
   );
 };
